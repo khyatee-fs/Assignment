99ed69b050c890c5a93e804e17856a0594de98bd
khyatee-fs <khyatipatel5696@gmail.com>
2024-06-09 14:10:13 +0530

    Assignment2

--- React-JS/assignment/src/App.jsx
+++ React-JS/assignment/src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ListView from './React-Assignment1/Practical/ListView'
 import Btnclick from './React-Assignment1/Practical/Btnclick'
+import ListandHook from './React-Assignment1/Practical/ListandHook'
+import Extra from './React-Assignment1/Practical/Extra'
 
 export default function App() {
   return (
     <div>
-      <ListView/>
+      {/* <ListView/>
       <br />
       <Btnclick/>
+      <br /> */}
+      {/* <ListandHook/> */}
+      <br />
+      <Extra/>
     </div>
   )
 }
